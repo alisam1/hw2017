7cd8a957de6950e3b599fa2ab6c8a6dd07319960
alisam1 <alisa-m2@yandex.ru>
2017-01-29 16:20:27 +0300

    CW02

---
+++ ClassWork2017/src/Intro/CW02.java
@@ -0,0 +1,10 @@
+package Intro;
+
+/**
+ * Created by mac on 29.01.17.
+ */
+public class CW02 {
+    static public void main (String arguments){
+        System.out.println("Hello world!");
+    }
+}
